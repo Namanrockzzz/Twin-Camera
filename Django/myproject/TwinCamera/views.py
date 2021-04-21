e048cda54dde5318751ce35ba87f1ddf03e7f597
kopal960 <kopalmsm@gmail.com>
2021-04-21 13:20:08 +0530

    add progress page

--- Django/myproject/TwinCamera/views.py
+++ Django/myproject/TwinCamera/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from time import sleep
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,9 @@ def page3(request):
     n = request.GET['n']
     n = int(n)
     return render(request, 'page3.html', {"n":[i for i in range(1,n+1)]})
+
+def processing(request , n=10):
+    return update_progress(request , 5, n)
+
+def update_progress(request, i, n):
+    return render(request , 'processing.html' , {'progress' : i/n*100})
